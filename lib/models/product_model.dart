1aa6332d13affbe7b6293a5b98b086f5a3af4f54
lamza13495@gmail.com <lamza13495@gmail.com>
2019-08-06 11:51:12 +0700

    listView

---
+++ lib/models/product_model.dart
@@ -0,0 +1,11 @@
+class ProductModel {
+
+
+  // Field
+  String name, detail,url;
+
+
+  // Construr
+  ProductModel(this.name,this.detail,this.url);
+  
+}
--- lib/screens/show_product.dart
+++ lib/screens/show_product.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:ung_ssru/models/product_model.dart';
 
 class ShowProduct extends StatefulWidget {
   @override
@@ -6,10 +11,105 @@ class ShowProduct extends StatefulWidget {
 }
 
 class _ShowProductState extends State<ShowProduct> {
+//Ex
+  Firestore firestore = Firestore.instance;
+  StreamSubscription<QuerySnapshot> subscription;
+  List<DocumentSnapshot> snapshots;
+  List<ProductModel> productmodels = [];
+
+//Method
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readFireStore();
+  }
+
+  Future<void> readFireStore() async {
+    CollectionReference collectionReference = firestore.collection('Product');
+    subscription = await collectionReference.snapshots().listen((dataSnapshot) {
+      snapshots = dataSnapshot.documents;
+
+      for (var snapshot in snapshots) {
+        String nameProduct = snapshot.data['Name'];
+        print('nameProduct = $nameProduct ');
+
+        String detailProduct = snapshot.data['Detail'];
+        String urlProduct = snapshot.data['Url'];
+
+        ProductModel productmodel =
+            ProductModel(nameProduct, detailProduct, urlProduct);
+
+        setState(() {
+          productmodels.add(productmodel);
+        });
+      }
+    });
+  }
+
+  Widget showImage(int index) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(30.0)),
+      height: 100.0,
+      width: 150.0,
+      child: Image.network(
+        productmodels[index].url,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  Widget showText(int index) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        showName(index),
+        showDetailShort(index),
+      ],
+    );
+  }
+
+  Widget showName(int index) {
+    return Text(
+      productmodels[index].name,
+      style: TextStyle(fontSize: 24.0),
+    );
+  }
+
+  Widget showDetailShort(int index) {
+    String detailShort = (productmodels[index].detail).substring(1, 70);
+    return Container(
+      width: 180.0,
+      child: Text('$detailShort ...'),
+    );
+  }
+
+  Widget showListProduct() {
+    return Container(
+          child: ListView.builder(
+        itemCount: productmodels.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(decoration: index %2 == 0 
+          ? BoxDecoration(color: Colors.green[300])
+          :BoxDecoration(color: Colors.green[50]),
+                      child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                showImage(index),SizedBox(width: 8.0,),
+                showText(index),
+                
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('This is ShowProduct'),
-    );
+    return Container(child: showListProduct());
   }
 }
